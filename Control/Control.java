990ac0d058be8d335f1418a645346c9d6fd9a403
Minituxfr <michaeldemesy@gmail.com>
2016-05-12 17:15:31 +0200

    Add files via upload

---
+++ Control/Control.java
@@ -0,0 +1,16 @@
+package Control;
+import View.*;
+import Model.*;
+
+
+/**
+ * Created by Michael on 12/05/2016.
+ */
+public class Control {
+    public Control(){
+        init();
+    }
+    public void init() {
+        Model m = new Model();
+    }
+}
---
+++ Launcher.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Michael on 12/05/2016.
+ */
+import Control.Control;
+public class Launcher {
+    public static void main (String[] args) {
+        Control c = new Control();
+    }
+}
