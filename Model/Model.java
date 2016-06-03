42b7edc16d56702de33131ddacdf34b9e6785198
Mcmouthy <dyland25@hotmail.fr>
2016-06-03 16:40:21 +0200

    Add files via upload

--- Model/Model.java
+++ Model/Model.java
@@ -5,18 +5,22 @@ import View.Grille;
 import View.Sst;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Timer;
+import java.util.*;
 
 /**
  * Created by PC-Dylan on 03/05/2016.
  */
 public class Model {
-
+    
     public Grille jeu;
     public Sst sst;
+
+    public static final int VIDE=0;
+    public static final int MUR=1;
+    public static final int SNACK=2;
+    public static final int TETE=3;
+    public static final int CORPS=4;
+    public static final int QUEUE=5;
 
     public static int taille_X;
     public static int taille_Y;
@@ -24,7 +28,7 @@ public class Model {
     private static int position_X; // position des differents objets en X
     private static int position_Y; // position des differents objets en Y
 
-    private static Case[][] grille;
+    private static int[][] grille;
     private static int score; // score du joueur actuel
     private static int size; // taille actuelle du serpent
     private static int vitesse; // vitesse de deplacementdu serpent en nombre de case du tableau
@@ -40,15 +44,15 @@ public class Model {
     private List<int[]> pos_Snack;
     protected Timer chrono;
     private int[] tps;
-    public long[] timeSST;
-    public long[] timeJEU;
+    private boolean enJeu;
 
     public Model(){ // constructeur qui initialise  tous les parametres a zero.
         position_X=0;
         position_Y=0;
+
         taille_X=40;
         taille_Y=40;
-        grille=new Case[taille_X][taille_Y];
+        grille=new int[taille_X][taille_Y];
         score=0;
         size=0;
         vitesse=100;
@@ -62,6 +66,7 @@ public class Model {
         pos_Snack= new ArrayList<>();
         chrono=new Timer();
         tps=new int[3];
+        enJeu=false;
     }
 
     /*------------------les setters----------------------*/
@@ -90,10 +95,11 @@ public class Model {
         if (tps>=0) this.temps=tps;
     }
 
-    public void setEnJeu(){
-        pause = !pause;
-    }
+    public void setPause(){pause = !pause;}
 
+    public void setEnJeu() {
+       enJeu= !enJeu;
+    }
 
     /*------------------les getters----------------------*/
 
@@ -121,12 +127,20 @@ public class Model {
         return this.temps;
     }
 
-    public boolean getEnPause(){
+    public boolean getPause(){
         return pause;
+    }
+
+    public boolean getEnJeu() {
+        return enJeu;
     }
 
     public List<int[]> getSerpent() {
         return serpent;
+    }
+
+    public static int[][] getGrille() {
+        return grille;
     }
 
     /*-----------------les calculs----------------------*/
@@ -136,47 +150,48 @@ public class Model {
         tabDir[0][1]=y;
     }
 
-    /* public void genereMur(Case[][] grille){
-         for (int i=0;i<taille_Y;i++) {
-             grille[0][i] = Case.MUR;
-             grille[taille_X - 1][i] = Case.MUR;
-         }
-         for (int i=0;i<taille_X;i++){
-             grille[i][0]=Case.MUR;
-             grille[i][taille_Y-1]=Case.MUR;
-         }
-     }
-     public void genereSnacks(Case[][] grille){
-         Random random=new Random();
-         int index_snacks_X= random.nextInt((taille_X-1)+1);
-         int index_snackq_Y= random.nextInt((taille_Y-1)+1);
-         if (grille[index_snacks_X][index_snackq_Y]!=Case.MUR){
-             grille[index_snacks_X][index_snackq_Y]=Case.SNACKS;
-         }else{
-             genereSnacks(grille);
-         }
-     }*/
-    /*-------------------------------------------------------------*/
-    /*--------------------gestion des scores-----------------------*/
-    public void initScore(){ // methode qui initialise les valeurs des scores dans tabScore
-        try {
-            BufferedReader record=new BufferedReader(new FileReader(new File("Record")));
-            String[] strtab=new String[10];
-            String buf;
-            for (int i=0;i<10;i++){
-                buf=record.readLine();
-                if (buf!=null) {
-                    strtab[i] = buf;
-                    tabScore[i]=strtab[i];
-                }
-
-            }
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+   /* public void genereMur(Case[][] grille){
+        for (int i=0;i<taille_Y;i++) {
+            grille[0][i] = Case.MUR;
+            grille[taille_X - 1][i] = Case.MUR;
+        }
+        for (int i=0;i<taille_X;i++){
+            grille[i][0]=Case.MUR;
+            grille[i][taille_Y-1]=Case.MUR;
         }
     }
+
+    public void genereSnacks(Case[][] grille){
+        Random random=new Random();
+        int index_snacks_X= random.nextInt((taille_X-1)+1);
+        int index_snackq_Y= random.nextInt((taille_Y-1)+1);
+        if (grille[index_snacks_X][index_snackq_Y]!=Case.MUR){
+            grille[index_snacks_X][index_snackq_Y]=Case.SNACKS;
+        }else{
+            genereSnacks(grille);
+        }
+    }*/
+    /*-------------------------------------------------------------*/
+    /*--------------------gestion des scores-----------------------*/
+     public void initScore(){ // methode qui initialise les valeurs des scores dans tabScore
+         try {
+             BufferedReader record=new BufferedReader(new FileReader(new File("Record")));
+             String[] strtab=new String[10];
+             String buf;
+             for (int i=0;i<10;i++){
+                 buf=record.readLine();
+                 if (buf!=null) {
+                     strtab[i] = buf;
+                     tabScore[i]=strtab[i];
+                 }
+
+             }
+         } catch (FileNotFoundException e1) {
+             e1.printStackTrace();
+         } catch (IOException e1) {
+             e1.printStackTrace();
+         }
+     }
 
     public boolean verifMeilleurScore(){ // methode qui verifie si il y a un score a changer
         String[] strtab= new String[10];
@@ -274,6 +289,16 @@ public class Model {
 
     /**/
     public void initGrille() {
-
+        for (int i=0;i<taille_X;i++){
+            for (int j=0;j<taille_Y;j++){
+                if (i==0 || i==taille_X-1 || j==0 || j==taille_Y-1){
+                    grille[i][j]=1;
+                }else {
+                    grille[i][j]=0;
+                }
+            }
+        }
     }
+
+
 }
