0f9cdee62115a7b33e1f8ac80707acc559d620cd
Mcmouthy <dyland25@hotmail.fr>
2016-06-06 17:27:50 +0200

    Add files via upload

--- Snake/src/View/Fenetre.java
+++ Snake/src/View/Fenetre.java
@@ -1,11 +1,15 @@
 package View;
 
-import Control.ControlSst;
 import Model.*;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
+import java.io.File;
 
 /**
  * Created by rydkey on 03/05/16.
@@ -21,13 +25,14 @@ public class Fenetre extends JFrame {
     public Grille jeu;
     public Sst sst;
     public JPanel display;
+    public Clip clip;
 
     public Fenetre(Model model){
         this.model= model;
         initAttributs();
         createJeu();
         pack();
-        setTitle("Sneaky Snake - appuyez sur Entré pour commencer");
+        setTitle("Snake - appuyez sur entrée pour commencer");
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(700,700));
@@ -81,5 +86,39 @@ public class Fenetre extends JFrame {
 
     public void affichePause(){
         JOptionPane.showMessageDialog(this,"Appuyez sur espace pour reprendre","Pause",JOptionPane.WARNING_MESSAGE);
+    }
+
+    public void refresh(){
+        all.remove(jeu);
+        all.remove(sst);
+        jeu=new Grille(model.taille_X,model.taille_Y,this,model);
+        sst=new Sst(model);
+        all.add(jeu);
+        all.add(sst);
+        setContentPane(all);
+    }
+    public void recommencer(){
+        all.remove(jeu);
+        all.remove(sst);
+        jeu=new Grille(model.taille_X,model.taille_Y,this,model);
+        sst=new Sst(model);
+        all.add(jeu);
+        all.add(sst);
+        playSound();
+        setContentPane(all);
+    }
+
+    public void playSound() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/musicSnake.WAV").getAbsoluteFile());
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            clip.loop(9999);
+
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
     }
 }
