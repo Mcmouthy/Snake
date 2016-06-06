edcafd3c692024b3df9523daa2fda509cc01feb7
Mcmouthy <dyland25@hotmail.fr>
2016-06-06 11:11:55 +0200

    Update ControlGroup.java

--- Snake/src/Control/ControlGroup.java
+++ Snake/src/Control/ControlGroup.java
@@ -5,12 +5,12 @@ import Model.*;
 
 public class ControlGroup {
 
-    public ControlGroup(Model Model){
-        Fenetre fenetre= new Fenetre(Model);
-        ControlKey controlKey = new ControlKey(fenetre, Model);
-        ControlMenuScore controlMenuScore = new ControlMenuScore(fenetre, Model);
-        ControlMenuReset controlMenuReset = new ControlMenuReset(fenetre, Model);
-        ControlSst controlSst = new ControlSst(fenetre, Model);
+    public ControlGroup(Model model){
+        Fenetre fenetre= new Fenetre(model);
+        ControlKey controlKey = new ControlKey(fenetre, model);
+        ControlMenuScore controlMenuScore = new ControlMenuScore(fenetre, model);
+        ControlMenuReset controlMenuReset = new ControlMenuReset(fenetre, model);
+        ControlSst controlSst = new ControlSst(fenetre, model);
         fenetre.setVisible(true);
     }
 }
